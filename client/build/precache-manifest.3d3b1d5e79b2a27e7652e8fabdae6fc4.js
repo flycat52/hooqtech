4b27cff226f7f6b44546885bd2a9b66afc646324
Rayna <flycat52@gmail.com>
2019-03-14 17:10:30 +0800

    fix deployment issue

---
+++ client/build/precache-manifest.3d3b1d5e79b2a27e7652e8fabdae6fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12ba15ba7c28bbb06ce6",
+    "url": "/static/js/main.85d5ac2c.chunk.js"
+  },
+  {
+    "revision": "94dd3c3a8a3fd1611ccc",
+    "url": "/static/js/2.09a492d7.chunk.js"
+  },
+  {
+    "revision": "12ba15ba7c28bbb06ce6",
+    "url": "/static/css/main.324ae036.chunk.css"
+  },
+  {
+    "revision": "94dd3c3a8a3fd1611ccc",
+    "url": "/static/css/2.673bb601.chunk.css"
+  },
+  {
+    "revision": "9366472544f3e1f3dc352098788416c8",
+    "url": "/index.html"
+  }
+];
